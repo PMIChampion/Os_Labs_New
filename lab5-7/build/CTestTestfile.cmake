0b452adaf0bfb2565c7c4640417d6c29c7ffa29b
Владислав Бурдинский <vladislavburdinskij@MacBook-Pro-Vladislav.local>
2024-12-09 21:53:50 +0300

    Lab5-7

---
+++ lab5-7/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vladislavburdinskij/Documents/OS_Labs/OS_labs/lab5-7
+# Build directory: /Users/vladislavburdinskij/Documents/OS_Labs/OS_labs/lab5-7/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(ZMQTests "/Users/vladislavburdinskij/Documents/OS_Labs/OS_labs/lab5-7/build/tests")
+set_tests_properties(ZMQTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/vladislavburdinskij/Documents/OS_Labs/OS_labs/lab5-7/CMakeLists.txt;38;add_test;/Users/vladislavburdinskij/Documents/OS_Labs/OS_labs/lab5-7/CMakeLists.txt;0;")
