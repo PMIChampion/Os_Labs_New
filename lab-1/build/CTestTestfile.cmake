f666457218d3067e46f7dc6be424a9faa2dcccae
PMIChampion <144058282+PMIChampion@users.noreply.github.com>
2024-12-23 01:10:12 +0300

    Delete lab-1 directory

--- lab-1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vladislavburdinskij/Documents/lab_OS/OS_labs/lab-1
-# Build directory: /Users/vladislavburdinskij/Documents/lab_OS/OS_labs/lab-1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Lab1Tests "/Users/vladislavburdinskij/Documents/lab_OS/OS_labs/lab-1/build/run_tests")
-set_tests_properties(Lab1Tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/vladislavburdinskij/Documents/lab_OS/OS_labs/lab-1/CMakeLists.txt;36;add_test;/Users/vladislavburdinskij/Documents/lab_OS/OS_labs/lab-1/CMakeLists.txt;0;")
--- lab-1/include/child.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef CHILD_HPP
-#define CHILD_HPP
-
-// Объявление функции calculation
-int calculation(int a, int b, int c);
-
-#endif // CHILD_HPP
